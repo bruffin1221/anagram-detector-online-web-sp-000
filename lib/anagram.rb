b57017ec17b3c4c67547ecec5318fcd20f706c3f
Bruce Ruffin <bruffin1221@gmail.com>
2019-11-10 13:44:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :words, :word
+
+  def initialize(words)
+    @words=words
+  end
+
+def match(word)
+   @word=word
+   @word.each_cons(1).map{|v| v.join.split('').sort}.select{|x| x==@words.split('').sort}.flatten.join.split
+ end
+
+ end
+
+listen=Anagram.new("listen")
+listen.words
+
+listen.match(%w(enlists google inlets banana))
